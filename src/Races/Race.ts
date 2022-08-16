58c4510dfd53e446b536eb3db73d0501d6638039
TH.ThiagoAvels <thiagoavels1@gmail.com>
2022-08-16 20:29:18 -0300

    feat: 2 Requisito Concluido, Message: "Segundo requisito averiguado e concluido"

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,6 +1,6 @@
 export default abstract class Race {
-  private readonly _name: string; 
-  private readonly _dexterity: number; 
+  protected readonly _name: string; 
+  protected readonly _dexterity: number; 
 
   constructor(name: string, dexterity: number) {
     this._name = name;
