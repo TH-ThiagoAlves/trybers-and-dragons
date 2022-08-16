8c696c97de095eb79d1075bc3db6c7d22412556f
TH.ThiagoAvels <thiagoavels1@gmail.com>
2022-08-15 22:54:41 -0300

    feat: Teste, Message: "teste do primeiro requisito"

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,10 @@
-// import Battle from './Battle';
+import Race from './Race';
+/*
+import Orc from './Orc';
+import Elf from './Elf';
+import Halfling from './Halfling';
+import Dwarf from './Dwarf';
+*/
 
-// export default Battle;
+export default Race;
+// export { Orc, Elf, Halfling, Dwarf };
